3a3d84a1642b91ae1b8b6c5d1da2a15c7d674420
charleypeng <charleypeng@gmail.com>
2026-04-28 00:15:24 +0800

    chore: update platform configurations for iOS, macOS and Android
    - Update Android namespace from com.example to com.charleypeng
    - Add iOS/macOS Podfile and Pod configurations
    - Update macOS Xcode project for CocoaPods integration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.igitea"
+    namespace = "com.charleypeng.igitea"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
